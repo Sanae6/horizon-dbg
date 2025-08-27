1cbce86cca3f310cb7ff87877186e1bf92fe9b4f
Aubrey Taylor <aubrey@hall.ly>
2025-08-26 21:35:48 -0600

    knownlayout on header

--- horizon-dbg-protocol/src/frame.rs
+++ horizon-dbg-protocol/src/frame.rs
@@ -1,6 +1,6 @@
-use zerocopy::{FromBytes, Immutable, IntoBytes};
+use zerocopy::{FromBytes, Immutable, IntoBytes, KnownLayout};
 
-#[derive(Clone, Copy, FromBytes, IntoBytes, Immutable)]
+#[derive(Clone, Copy, FromBytes, IntoBytes, Immutable, KnownLayout)]
 #[repr(C)]
 pub struct Header {
   pub length: u16,
